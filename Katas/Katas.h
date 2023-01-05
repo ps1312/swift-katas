49a6f4b8f7c28e71645f7b191d3ba0227a53fdf0
Paulo Sérgio <ps131296@gmail.com>
2023-01-05 09:15:10 -0300

    Initial Commit

---
+++ Katas/Katas.h
@@ -0,0 +1,18 @@
+//
+//  Katas.h
+//  Katas
+//
+//  Created by Paulo Sergio da Silva Rodrigues on 05/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Katas.
+FOUNDATION_EXPORT double KatasVersionNumber;
+
+//! Project version string for Katas.
+FOUNDATION_EXPORT const unsigned char KatasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Katas/PublicHeader.h>
+
+
