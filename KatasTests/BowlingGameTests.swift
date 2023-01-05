80c3cf103f08ce240d791478828234bfca143303
Paulo Sérgio <ps131296@gmail.com>
2023-01-05 13:34:28 -0300

    add bowling scoring rules

---
+++ KatasTests/BowlingGameTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+/*
+
+ Bowling Rules
+ - a game consists of 10 frames
+ - in each frame, 2 rolls for each
+ - in each roll, the bowler can down 10 pins
+
+ - the score of the frame is the total number of pins knocked down (MAX 10) + bonuses:
+    - spare(10 pins in two rolls) : + next roll
+    - strike(10 pins in a single roll) : + next 2 rolls
+
+ - the bowler gets an additional roll if a spare is earned and two if a strike is earned on the 10th frame
+
+*/
+
+class BowlingGameTests: XCTestCase {
+
+}
