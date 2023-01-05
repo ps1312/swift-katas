d0ec5f1edf0038af643d1fc296ef4fabef9e21ba
Paulo Sérgio <ps131296@gmail.com>
2023-01-05 09:21:14 -0300

    add `FizzBuzz` instructions

---
+++ KatasTests/FizzBuzzTests.swift
@@ -0,0 +1,8 @@
+/*
+ Write a program using TDD that prints the numbers from 1 to 100.
+
+ For multiples of 3, print "Fizz"
+ For multiples of 5, print "Buzz"
+ For multiples of booth, print "FizzBuzz"
+*/
+
