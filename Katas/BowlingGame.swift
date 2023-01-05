0b2e49a48a09a57e715b3052dd4d2226fb97f07f
Paulo Sérgio <ps131296@gmail.com>
2023-01-05 14:41:50 -0300

    move `Game` to production

---
+++ Katas/BowlingGame.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+/*
+
+ Bowling Rules
+ - a game consists of 10 frames
+ - in each frame, 2 rolls for each
+ - in each roll, the bowler can down 10 pins
+
+ - the score of the frame is the total number of pins knocked down (MAX 10) + bonuses:
+    - spare(10 pins in two rolls) : + next roll
+    - strike(10 pins in a single roll) : + next 2 rolls
+
+ - the bowler gets an additional roll if a spare is earned and two if a strike is earned on the 10th frame
+
+*/
+
+public final class Game {
+    var attemps = [Int]()
+
+    public init() {}
+
+    public func roll(pins: Int) {
+        attemps.append(pins)
+    }
+
+    public func score() -> Int {
+        var score = 0
+
+        for frame in stride(from: 0, to: 20, by: 2) {
+            let frameScore = getFrameScore(frame: frame)
+            score += frameScore
+
+            if frameScore == 10 { // is spare, sum the next frame first roll for extra points
+                let extraScore = attemps[frame + 2]
+                score += extraScore
+            }
+        }
+
+        return score
+    }
+
+    private func getFrameScore(frame: Int) -> Int {
+        attemps[frame] + attemps[frame + 1]
+    }
+
+}
