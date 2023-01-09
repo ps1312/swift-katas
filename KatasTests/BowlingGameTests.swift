935c4b46516cdba693c18d2b6d0883c27a8dee7f
Paulo Sérgio <ps131296@gmail.com>
2023-01-09 09:20:28 -0300

    rename param to give better context

--- KatasTests/BowlingGameTests.swift
+++ KatasTests/BowlingGameTests.swift
@@ -15,7 +15,7 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         sut.roll(pins: 1)
-        rollMany(pins: 0, for: sut, left: 19)
+        rollMany(pins: 0, for: sut, remaining: 19)
 
         XCTAssertEqual(sut.score(), 1)
     }
@@ -24,7 +24,7 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         sut.roll(pins: 10)
-        rollMany(pins: 0, for: sut, left: 19)
+        rollMany(pins: 0, for: sut, remaining: 19)
 
         XCTAssertEqual(sut.score(), 10)
     }
@@ -33,7 +33,7 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         rollSpare(for: sut)
-        rollMany(pins: 0, for: sut, left: 18)
+        rollMany(pins: 0, for: sut, remaining: 18)
 
         XCTAssertEqual(sut.score(), 10)
     }
@@ -42,10 +42,8 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         rollSpare(for: sut)
-
         sut.roll(pins: 1)
-
-        rollMany(pins: 0, for: sut, left: 17)
+        rollMany(pins: 0, for: sut, remaining: 17)
 
         XCTAssertEqual(sut.score(), 12)
     }
@@ -54,10 +52,8 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         rollSpare(for: sut)
-
         rollFrame(for: sut, firstRoll: 0, secondRoll: 1)
-
-        rollMany(pins: 0, for: sut, left: 16)
+        rollMany(pins: 0, for: sut, remaining: 16)
 
         XCTAssertEqual(sut.score(), 11)
     }
@@ -66,10 +62,8 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         sut.roll(pins: 10)
-
         rollFrame(for: sut, firstRoll: 1, secondRoll: 0)
-
-        rollMany(pins: 0, for: sut, left: 16)
+        rollMany(pins: 0, for: sut, remaining: 16)
 
         XCTAssertEqual(sut.score(), 12)
     }
@@ -78,10 +72,8 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         sut.roll(pins: 10)
-
         rollFrame(for: sut, firstRoll: 0, secondRoll: 1)
-
-        rollMany(pins: 0, for: sut, left: 16)
+        rollMany(pins: 0, for: sut, remaining: 16)
 
         XCTAssertEqual(sut.score(), 12)
     }
@@ -90,10 +82,8 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         sut.roll(pins: 10)
-
         rollFrame(for: sut, firstRoll: 1, secondRoll: 1)
-
-        rollMany(pins: 0, for: sut, left: 16)
+        rollMany(pins: 0, for: sut, remaining: 16)
 
         XCTAssertEqual(sut.score(), 14)
     }
@@ -102,10 +92,8 @@ class BowlingGameTests: XCTestCase {
         let sut = Game()
 
         rollSpare(for: sut)
-
         sut.roll(pins: 10)
-
-        rollMany(pins: 0, for: sut, left: 16)
+        rollMany(pins: 0, for: sut, remaining: 16)
 
         XCTAssertEqual(sut.score(), 30)
     }
@@ -128,8 +116,8 @@ class BowlingGameTests: XCTestCase {
         XCTAssertEqual(sut.score(), 155)
     }
 
-    private func rollMany(pins: Int, for sut: Game, left: Int = 20) {
-        for _ in 1...left {
+    private func rollMany(pins: Int, for sut: Game, remaining: Int = 20) {
+        for _ in 1...remaining {
             sut.roll(pins: pins)
         }
     }
